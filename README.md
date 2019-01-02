32b9fab94a12e61e08e31c58feae1b596b3ef7de
sebastianwilczek <s.wilczek@student.fontys.nl>
2019-01-02 14:51:24 +0100

    Finished endpoints

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # raspberry_pi_ttn_rest_server
 Raspberry Pi REST Server to retrieve information previously stored from The Thing Network
+
+## Installation and Start
+To install the required modules, run the following command after cloning this repository:
+```
+npm install
+```
+
+Once all the modules have been installed, the REST Server can be started with this command:
+```
+npm start
+```
+Following that, the REST Server can be reached at port 8081.
--- server.js
+++ server.js
@@ -1,30 +1,78 @@
 var express = require('express');
 
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+//var url = "mongodb://localhost:27017/";
+//var url = "mongodb://<USER>:<PASSWORD>@ds145484.mlab.com:45484/ttn";
+var url = "mongodb://TTNUser:TTNPassword1@ds145484.mlab.com:45484/ttn";
 
 app.get('/devices', function (req, res) {
   console.log("Incoming request at endpoint /devices");
 
-  res.end("RESULT /devices");
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("ttn");
+  
+    dbo.collection("deviceLog").find({}, { projection: { _id: 0, device_id: 1 } }).toArray(function(err, result) {
+      if (err) throw err;
+      db.close();
 
-  console.log("Request ended at endpoint /devices");
+      var resultArray = [];
+      result.forEach(function(entry) {
+        resultArray.indexOf(entry.device_id) === -1 ? resultArray.push(entry.device_id) : console.log("Duplicate of Device ID found, discarding");
+      });
+
+      console.log("Result:");
+      console.log(resultArray);
+      res.writeHead(200, { "Content-Type": "application/json"});
+      res.write(JSON.stringify(resultArray));
+      res.end();
+      console.log("Request ended at endpoint /devices\n");
+    });
+  });
 });
 
 app.get('/entries', function (req, res) {
   console.log("Incoming request at endpoint /entries");
 
-  res.end("RESULT /entries");
-
-  console.log("Request ended at endpoint /entries");
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("ttn");
+  
+    dbo.collection("deviceLog").find({}, { projection: { _id: 0 } }).toArray(function(err, result) {
+      if (err) throw err;
+      console.log("Result:");
+      console.log(result);
+      db.close();
+      res.writeHead(200, { "Content-Type": "application/json"});
+      res.write(JSON.stringify(result));
+      res.end();
+      console.log("Request ended at endpoint /entries\n");
+    });
+  });
 });
 
 app.get('/entries/:deviceId', function (req, res) {
   var deviceId = req.params.deviceId;
   console.log("Incoming request at endpoint /entries, Device ID is %s", deviceId);
 
-  res.end("RESULT /entries/" + deviceId);
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("ttn");
+
+    var query = { device_id: deviceId };
   
-  console.log("Request ended at endpoint /entries for Device ID %s", deviceId);
+    dbo.collection("deviceLog").find(query, { projection: { _id: 0 } }).toArray(function(err, result) {
+      if (err) throw err;
+      console.log("Result:");
+      console.log(result);
+      db.close();
+      res.writeHead(200, { "Content-Type": "application/json"});
+      res.write(JSON.stringify(result));
+      res.end();
+      console.log("Request ended at endpoint /entries for Device ID %s\n", deviceId);
+    });
+  });
 });
 
 var server = app.listen(8081, function () {
